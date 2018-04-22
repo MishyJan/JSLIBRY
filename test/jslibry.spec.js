c29047501715a4b5a5a17d1b3fda4d33e99528fc
zhangxu <2590000714@qq.com>
2018-04-22 16:57:23 +0800

    add tow function

--- test/jslibry.spec.js
+++ test/jslibry.spec.js
@@ -1,13 +1,30 @@
 const expect = require('chai').expect
 
-const cart = require('../lib/jslibry/jslibry')
-var cartTest = new cart()
+const Jslibry = require('../lib/jslibry')
+let jslibry = new Jslibry()
 
-describe('Cart', function () {
-    describe('subtotal', function () {
-        it('单价是 10 块钱的 3 件商品小计金额应该是 30 块', function () {
-            var subtotal = cartTest.subtotal(10, 3)
-            expect(subtotal).to.equal(30)
+describe('Jslibry', function () {
+    describe('字符串提取', function () {
+        it('"A: 1,2,3,4"：字符串提取为[1,2,3,4]', function () {
+            let toList = jslibry.collectStrToList();
+            [1, 2, 3, 4].forEach((ele, inx) => {
+                expect(toList[inx]).to.be.equal(ele);
+            });
         })
-    })
+    });
+
+    describe('检查字符串是否存在，并且非空', function () {
+        it('字符串不存在的情况', function () {
+            const result = jslibry.checkStrIsEmpty();
+            expect(result).to.not.be.ok;
+        })
+        it('字符串为空的情况', function () {
+            const result = jslibry.checkStrIsEmpty('');
+            expect(result).to.not.be.ok;
+        })
+        it('字符串不为空的情况', function () {
+            const result = jslibry.checkStrIsEmpty('HELLO WORLD!');
+            expect(result).to.be.ok;
+        })
+    });
 })
